2c5db56137de47d5ec4776d0b5183a29f7adb52a
vitorTB <vitortoldato4@gmail.com>
2023-07-31 22:41:56 -0300

    atulizandoph

---
+++ PHP/Apendrizado_Formulario/login.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+$error="";
+if(!isset($_POST["login"]) or ($_POST["login"]==""))
+$error = "Preencha o Login";
+console.log("error");
+elseif(
+    !isset($_POST["senha"] ) or ($_POST["senha"] ==""))
+    $error = "Preencha a senha";
+else{
+    $login=$_POST["login"];
+    $senha=$_POST["senha"];
+    if($login!="admin" or $senha!="1234"){
+        $error="login ou senha invalidos";
+    }else{
+        $_SESSION["usuario"] ="Administrador";
+    }
+}
+if($error!="")
+header("Location: form_login.php?error=$error", true, 301);
+else
+header("Location: protegida.php", true, 301);
+
+?>
